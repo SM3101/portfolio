e933ca048d4478cb0ad5f0e287b5d49667846a2e
SM3101 <148704388+SM3101@users.noreply.github.com>
2023-10-22 11:03:39 -0400

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from . models import Message
+
+# Create your views here.
+def homepage(request):
+
+    if request.method == "POST":
+        name = request.POST['user_name']
+        email = request.POST['emailaddress']
+        phonenumber = request.POST['phonenumber']
+
+        new_message = Message(name=name, email=email, phone=phonenumber)
+        new_message.save()
+
+    return render(request, 'home.html')
